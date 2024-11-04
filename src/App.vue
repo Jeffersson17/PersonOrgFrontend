39f205c62876ecfac3076a7693b672c13714ad21
Jeffersson <barrosjefferson407@gmail.com>
2024-11-04 18:54:57 -0300

    Adição do Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
 
-</script>
+export default {
+  name: 'App',
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
 }
-</style>
+</script>
