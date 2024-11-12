7df7f4e2e3df1d40a9a9d5d9e0457bd75b72beb9
Jeffersson <barrosjefferson407@gmail.com>
2024-11-12 17:00:09 -0300

    Adição da NavBar com um dropdown para Pessoa

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,35 @@
     <v-app>
       <v-main>
         <div class="container" id="home">
+          <v-app-bar color="primary" dark>
+            <v-app-bar-title>Meu Projeto</v-app-bar-title>
+            <v-spacer></v-spacer>
+
+          <!-- Menu suspenso para Pessoa -->
+          <v-menu
+            open-on-hover
+          >
+            <template v-slot:activator="{ props }">
+              <v-btn v-bind="props">
+                Pessoa
+              </v-btn>
+            </template>
+
+            <v-list>
+              <v-list-item>
+                <router-link to="/" class="v-list-item-title">
+                  <v-btn text>Home</v-btn>
+                </router-link>
+              </v-list-item>
+
+              <v-list-item>
+                <router-link to="/create" class="v-list-item-title">
+                  <v-btn text>Registrar Usuário</v-btn>
+                </router-link>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-app-bar>
           <router-view></router-view>
 
           <footer class="footer">
