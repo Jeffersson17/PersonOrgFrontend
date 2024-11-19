847b43856bdd47f91c835f490a8e96998d0ed423
Jeffersson <barrosjefferson407@gmail.com>
2024-11-19 14:50:36 -0300

    Mudanças no RegisterOrg

--- src/components/Organization/RegisterOrg.vue
+++ src/components/Organization/RegisterOrg.vue
@@ -2,7 +2,7 @@
     <div>
         <div id="organization">
             <h5>ORGANIZAÇÃO</h5>
-            <v-form id="form-organization">
+            <v-form id="form-organization" @submit.prevent="submitOrg">
                 <v-container>
                     <v-row>
                         <v-col
@@ -10,7 +10,9 @@
                             md="6"
                         >
                             <v-text-field
+                                v-model="name"
                                 label="Nome"
+                                variant="outlined"
                                 required
                             ></v-text-field>
                         </v-col>
@@ -20,7 +22,9 @@
                             md="6"
                         >
                             <v-text-field
+                                v-model="phone"
                                 label="Telefone"
+                                variant="outlined"
                                 required
                             ></v-text-field>
                         </v-col>
@@ -30,7 +34,11 @@
                             md="6"
                         >
                             <v-combobox
+                                v-model="selectedArea"
                                 label="Area de Atuação"
+                                :items="areaItems"
+                                variant="outlined"
+                                required
                             ></v-combobox>
                         </v-col>
 
@@ -39,7 +47,13 @@
                             md="6"
                         >
                             <v-combobox
+                                v-model="selectedAddress"
                                 label="Endereço"
+                                :items="addressItems"
+                                item-value="id"
+                                item-text="address"
+                                variant="outlined"
+                                required
                             ></v-combobox>
                         </v-col>
 
@@ -48,102 +62,24 @@
                             md="12"
                         >
                             <v-textarea
-                                label="Descrição">
-                            </v-textarea>
+                                v-model="description"
+                                variant="outlined"
+                                label="Descrição"
+                            ></v-textarea>
+                        </v-col>
+
+                        <v-col
+                            cols="12"
+                            md="12"
+                        >
+                            <v-btn
+                                class="me-4"
+                                type="submit"
+                                variant="tonal"
+                                id="btn-org"
+                            >submit</v-btn>
                         </v-col>
                     </v-row>
-
-                    <h5>ENDEREÇO</h5>
-                    <v-col
-                        cols="12"
-                        md="5"
-                    >
-                        <v-text-field
-                            label="Endereço"
-                            v-model="address"
-                            required
-                        ></v-text-field>
-                    </v-col>
-
-                    <v-col
-                        cols="12"
-                        md="4"
-                    >
-                        <v-combobox
-                            label="Tipo de Endereço"
-                            v-model="address_type"
-                        ></v-combobox>
-                    </v-col>
-
-                    <v-col
-                        cols="12"
-                        md="3"
-                    >
-                        <v-text-field
-                            label="Numero"
-                            v-model="number"
-                            required
-                        ></v-text-field>
-                    </v-col>
-
-                    <v-col
-                        cols="12"
-                        md="6"
-                    >
-                        <v-combobox
-                            label="Cidade"
-                            v-model="city"
-                        ></v-combobox>
-                    </v-col>
-
-                    <v-col
-                        cols="12"
-                        md="6"
-                    >
-                        <v-text-field
-                            label="CEP"
-                            v-model="cep"
-                            required
-                        ></v-text-field>
-                    </v-col>
-
-                    <v-col
-                        cols="12"
-                        md="12"
-                    >
-                        <v-textarea
-                            label="Complemento ( Opcional )"
-                            v-model="complement"
-                        ></v-textarea>
-                    </v-col>
-
-                    <h5>CIDADE</h5>
-                    <v-col
-                        cols="12"
-                        md="6"
-                    >
-                        <v-text-field
-                            label="Nome da Cidade"
-                            v-model="city"
-                            required
-                        ></v-text-field>
-                    </v-col>
-
-                    <v-col
-                        cols="12"
-                        md="6"
-                    >
-                        <v-combobox
-                            label="Estado"
-                            v-model="state"
-                        ></v-combobox>
-                    </v-col>
-
-                    <v-btn
-                        class="me-4"
-                        type="submit"
-                        id="btn-org"
-                    >submit</v-btn>
                 </v-container>
             </v-form>
         </div>
@@ -155,21 +91,62 @@ import { http } from '@/services/config';
 import { onMounted, ref } from 'vue';
 
 const areaItems = ref([]);
+const addressItems = ref([]);
+const name = ref(null);
+const phone = ref(null);
+const description = ref(null);
+const selectedAddress = ref(null);
+const selectedArea = ref(null);
 
 function areaChoice() {
     http.get('/api/choices')
         .then(response => {
             if(response.data && response.data.area_choices) {
                 areaItems.value = response.data.area_choices;
+                console.log(response.data);
             }
         })
         .catch(error => {
-            console.log(error.message);
+            console.log('Erro ao buscar as areas: ', error.message);
+        })
+}
+
+function addressChoice() {
+    http.get('/api/address/')
+        .then(response => {
+            if(response.data) {
+                addressItems.value = response.data.map(address => address.address);
+            }
+        })
+        .catch(error => {
+            console.log('Erro na requisição: ', error.message);
+        })
+}
+
+function submitOrg() {
+    const addressData = {
+        id: selectedAddress.value.id
+    }
+
+    http.post('/api/organizations/', {
+        name: name.value,
+        phone: phone.value,
+        area: selectedArea.value,
+        description: description.value,
+        address_base: addressData
+    })
+        .then(() => {
+            console.log('Enviado com sucesso!')
+        })
+        .catch(error => {
+            console.log('Erro ao enviar o cadastro: ', error.response?.data || error.message);
+            console.log(addressData);
         })
 }
 
 onMounted(() => {
     areaChoice();
+    addressChoice();
 });
 </script>
 
